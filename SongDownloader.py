6a53321fe29437c3f2a81c01fd37329745a01b8f
Howie-Ubuntu <howiechi@163.com>
2016-02-05 15:38:56 +0800

    SongDownloader.py Modified

--- SongDownloader.py
+++ SongDownloader.py
@@ -6,7 +6,7 @@ class SongDownloader():
         self.host = 'http://tingapi.ting.baidu.com/v1/restserver/ting'
 	self.payload = {
             'format': fmt,
-            'callback': '',
+            'calback': '',
             'from': 'webapp_music'
         }
 
@@ -54,19 +54,21 @@ class SongDownloader():
             payload = dict(payload, **self.payload)
             r = requests.get(self.host, params=payload)
             jsonObj = r.json()
-            bitlinks = [(int(each['file_bitrate']), each['file_link']) 
+            bitlinks = [(each['file_bitrate'], each['file_link'], each['file_extension']) 
                         for each in jsonObj['bitrate'] if each['file_link'] != '']
             if len(bitlinks) == 0:
-                bitlinks = [(int(each['file_bitrate']), each['show_link']) 
+                bitlinks = [(each['file_bitrate'], each['show_link'], each['file_extension']) 
                             for each in jsonObj['bitrate'] if each['show_link'] != ''
                             and ('mp3' in each['show_link'] or 'flac' in each['show_link'])]
                 if len(bitlinks) == 0:
                     print 'No links found'
                     exit(0)
-            link = sorted(bitlinks)[-1][1]
+            link, extension = sorted(bitlinks, key=lambda x: 640 if x[0]=='flac' else int(x[0]))[-1][1:]
+            if extension == '':
+                extension = 'mp3'
             try:
                 print 'Downloading...'
-                urllib.urlretrieve(link, './%s.mp3' % song)
+                urllib.urlretrieve(link, './%s.%s' % (song, extension))
                 print 'Download finished.'
             except Exception as e:
                 print 'Download failed.'
